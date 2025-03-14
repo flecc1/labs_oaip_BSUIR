5717f44fb710edb8e7872ff41d340059e5638665
Egor_Lesckovsckey <egor.fortnite.god@gmail.com>
2025-03-15 00:54:57 +0300

    )

---
+++ labs_2SEM/laba3/functions.c
@@ -0,0 +1,48 @@
+#include "header.h"
+
+struct Stack *StackInit(struct Stack *top, char data)
+{
+    struct Stack *ptr = (struct Stack*)malloc(sizeof(struct Stack));
+    ptr->data = data;
+    ptr->next = top;
+    return ptr;
+}
+
+struct Stack *pop(struct Stack *top)
+{
+    if(top == NULL)
+    {
+        return top;
+    }
+    struct Stack *ptr_next = top->next;
+    free(top);
+    return ptr_next;
+}
+
+
+
+void showStack(struct Stack *top)
+{
+    if(top == NULL)
+    {
+        printf("стэк пуст \n");
+        return;
+    }
+    while(top)
+    {
+        printf("%d ", top->data);
+        top = top->next;
+    }
+    printf("\n");
+}
+
+
+void peek(struct Stack *top)
+{
+    if(top == NULL)
+    {
+        printf("стэк пуст\n");
+        return;
+    }
+    printf("%d\n", top->data);
+}
