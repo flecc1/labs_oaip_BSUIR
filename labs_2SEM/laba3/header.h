a91cd6ecd1eeca1b1099b331a85f6988a0ac5915
Egor_Lesckovsckey <egor.fortnite.god@gmail.com>
2025-03-15 01:06:28 +0300

    доавлен README, раскидал все по файлам

--- labs_2SEM/laba3/header.h
+++ labs_2SEM/laba3/header.h
@@ -11,49 +11,7 @@ struct Stack
 };
 
 
-struct Stack *StackInit(struct Stack *top, char data)
-{
-    struct Stack *ptr = (struct Stack*)malloc(sizeof(struct Stack));
-    ptr->data = data;
-    ptr->next = top;
-    return ptr;
-}
-
-struct Stack *pop(struct Stack *top)
-{
-    if(top == NULL)
-    {
-        return top;
-    }
-    struct Stack *ptr_next = top->next;
-    free(top);
-    return ptr_next;
-}
-
-
-
-void showStack(struct Stack *top)
-{
-    if(top == NULL)
-    {
-        printf("стэк пуст \n");
-        return;
-    }
-    while(top)
-    {
-        printf("%d ", top->data);
-        top = top->next;
-    }
-    printf("\n");
-}
-
-
-void peek(struct Stack *top)
-{
-    if(top == NULL)
-    {
-        printf("стэк пуст\n");
-        return;
-    }
-    printf("%d\n", top->data);
-}
+struct Stack *StackInit(struct Stack *top, char data);
+struct Stack *pop(struct Stack *top);
+void showStack(struct Stack *top);
+void peek(struct Stack *top);
