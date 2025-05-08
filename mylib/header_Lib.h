daab1df18ef7e5f36a921d99a60197f4dddbc504
Egor_Lesckovsckey <zitreezyx@gmail.com>
2025-05-08 23:21:05 +0300

    добавлены вцеа в header

--- mylib/header_Lib.h
+++ mylib/header_Lib.h
@@ -5,6 +5,15 @@
 #include <ctype.h>
 
 
+// Цвета для терминала
+#define COLOR_RED     "\x1b[31m"
+#define COLOR_GREEN   "\x1b[32m"
+#define COLOR_YELLOW  "\x1b[33m"
+#define COLOR_BLUE    "\x1b[34m"
+#define COLOR_MAGENTA "\x1b[35m"
+#define COLOR_CYAN    "\x1b[36m"
+#define COLOR_RESET   "\x1b[0m"
+
 int getValidInt(int min, int max);
 void readString(char *buffer, int max_length);
 float getValidFloat(int min, int max);
